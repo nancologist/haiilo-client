f3338e9a7d5cda2f1bc2e4e0a0d237ae9b64ace5
morteza <nancologist@gmail.com>
2025-07-23 20:09:11 +0200

    Add order cmp and checkout

--- src/app/components/order/order.component.ts
+++ src/app/components/order/order.component.ts
@@ -3,21 +3,31 @@ import {OrderService} from '../../services/order.service';
 import {AsyncPipe, NgForOf, NgIf} from '@angular/common';
 import {Observable} from 'rxjs';
 import {OrderItem} from '../../../types';
+import {ButtonComponent} from '../button/button.component';
 
 @Component({
   selector: 'app-order',
   imports: [
     NgIf,
     AsyncPipe,
-    NgForOf
+    NgForOf,
+    ButtonComponent
   ],
   templateUrl: 'order.component.html',
   styleUrl: 'order.component.css'
 })
 export class OrderComponent {
-  orderItems$: Observable<OrderItem[]>
+  orderItems$: Observable<OrderItem[]>;
 
-  constructor(private orderService: OrderService) {
-    this.orderItems$ = this.orderService.orderItems$
+  constructor(protected orderService: OrderService) {
+    this.orderItems$ = this.orderService.orderItems$;
+  }
+
+  checkout(): void {
+    this.orderService.checkout();
+  }
+
+  resetOrder(): void {
+    this.orderService.clearOrder();
   }
 }
--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,16 +1,22 @@
 import {Injectable} from '@angular/core';
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, catchError, map, Observable, of} from 'rxjs';
 import {ItemScanEvent, OrderItem} from '../../types';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
   private orderItems = new BehaviorSubject<OrderItem[]>([]);
+  // private sum = new BehaviorSubject<number>(0)
   public readonly orderItems$ = this.orderItems.asObservable();
+  public sum$ : Observable<number> | undefined;
 
+  constructor(private http: HttpClient) {
+  }
 
   addItem(event: ItemScanEvent): void {
+    this.sum$ = undefined;
     const currentOrder = this.orderItems.getValue();
     const index = currentOrder.findIndex(order => order.itemId == event.itemId);
     const itemAlreadyInOrder = index > -1;
@@ -29,5 +35,26 @@ export class OrderService {
       };
       this.orderItems.next([...currentOrder, newOrderItem]);
     }
+  }
+
+  checkout() {
+    const data = {
+      cart: this.orderItems.getValue().map(({ itemId, quantity }) => ({ itemId, quantity }))
+    }
+    this.sum$ = this.http.post<{ sum: number }>("http://localhost:8080/checkout", data).pipe(
+      map((res) => res.sum),
+      catchError(err => {
+        if (err instanceof  Error) {
+          console.error(`An error occurred: ${err.message}`)
+        }
+        console.error("Unknown error!");
+        return of()
+      })
+    )
+  }
+
+  clearOrder(): void {
+    this.sum$ = undefined;
+    this.orderItems.next([]);
   }
 }
